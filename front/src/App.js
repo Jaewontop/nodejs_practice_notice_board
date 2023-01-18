548748f6d3fb3abfe80d9ea879c72946512dd1eb
0912078 <0912078@naver.com>
2023-01-18 12:11:47 +0900

    Feat: front에 버튼에 post 붙임임

--- front/src/App.js
+++ front/src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
+import React, { useState } from "react";
+
+const URL = "localhost:3001";
 
 function App() {
+  const comments = ["너무 좋아요우ㅜㅜ", "행복해요!!"];
+  const sendComment = async (content) => {
+    axios
+      .post(URL, {
+        content: content,
+      })
+      .then(function (response) {
+        console.log("[DEBUG] response:" + response);
+      })
+      .catch(function (error) {
+        console.log("[DEBUG] error:" + error);
+      })
+      .then(function () {
+        // 항상 실행
+      });
+  };
+  const [text, setText] = useState("");
+
+  const onChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onReset = () => {
+    setText("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>위로의 말</h1>
+      <div className="blank"></div>
+      <div id="content">
+        {comments.map((comment) => {
+          return <p> {comment} </p>;
+        })}
+      </div>
+      <div className="blank"></div>
+      <div id="input_box">
+        <input onChange={onChange} placeholder="따뜻한 말을 건네주세요" />
+        <div
+          onClick={() => {
+            sendComment(text);
+            onReset();
+          }}
         >
-          Learn React
-        </a>
-      </header>
+          +
+        </div>
+      </div>
     </div>
   );
 }
