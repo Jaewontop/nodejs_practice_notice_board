3cc4d31506284ed24a08c952f1d1fbb43594f000
0912078 <0912078@naver.com>
2023-01-18 12:01:01 +0900

    Feat: mysql 연동 및 테스트

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const mysql = require("mysql");
 
 app.listen(3001, function () {
   console.log("listening on 3001");
@@ -11,3 +12,26 @@ app.use(express.static(path.join(__dirname, "../front/build")));
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "../front/build/index.html"));
 });
+
+app.get("/comments", async (req, res) => {
+  res.send(req);
+});
+
+app.post("/comments", async (req, res) => {
+  const sqlQuery = `insert into test.user(userid, content) values (${req}, '코드기어');`;
+});
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  user: "test",
+  password: "test",
+  database: "test",
+});
+connection.connect();
+
+connection.query("SELECT * from USER", function (error, results, fields) {
+  if (error) throw error;
+  console.log("users: ", results);
+});
+
+connection.end();
